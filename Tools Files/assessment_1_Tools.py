6e3234ec63c4e7990f3221cffad981cee1ad4e99
EliPreston <eli.preston@ucc.on.ca>
2020-10-06 15:08:39 -0400

    assessment stuff

--- Tools Files/assessment_1_Tools.py
+++ Tools Files/assessment_1_Tools.py
@@ -15,98 +15,39 @@ Post-conditions; The two passed lists remain unchanged.
 '''
 
 
-# def mergeStrings(a, b):
+def mergeStrings(a, b):
 
-#     c = []
+    c = []
 
-#     if len(a) == len(b):
-#         for i in range(len(a)):
-#             c.append(a[i] + b[i])
+    if len(a) == len(b):
+        for i in range(len(a)):
+            c.append(a[i] + b[i])
 
-#     else:
+    else:
 
-#         longer = a
-#         shorter = b
+        longer = a
+        shorter = b
 
-#         if len(b) > len(a):
-#             longer = b
-#             shorter = a
+        if len(b) > len(a):
+            longer = b
+            shorter = a
 
-#         count = 0
-#         while count < len(shorter):
-#             c.append(longer[count] + shorter[count])
-#             count += 1
+        count = 0
+        while count < len(shorter):
+            c.append(longer[count] + shorter[count])
+            count += 1
 
-#         for i in range(len(shorter), len(longer), 1):
-#             c.append(longer[i])
+        for i in range(len(shorter), len(longer), 1):
+            c.append(longer[i])
 
-#     return c
-
-
-# # Test cases
-# print(mergeStrings(["cat", "dog", "fish"], ["one", "two", "three"]))
-# print(mergeStrings(["cat", "dog", "fish"], ["one"]))
-# print(mergeStrings(["cat", "dog"], ["one", "two"]))
-# print(mergeStrings([], ["one", "two", "three"]))
+    return c
 
 
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-# #
-
-# '''
-# Description; The method takes an array of strings (grocery items in this case) and
-# a String and searches the array for a string already in the array that matches it.
-# If it finds a match, it returns an affirmation that the item is in the array at
-# index n. If not, it returns that the item is not in the array.
-
-# Parameters; Array array, String s
-
-# Return; String ("Item is not on your grocery list." // or // "Item is on grocery list at index " + sL)
-
-# Pre-conditions; array contains only strings, lowercase ,
-
-# Post-conditions;
-
-# '''
-
-
-# def findS(array, s):
-#     # Linear search for a string:
-#     # for i in array:
-#     #     i.lower()
-
-#     if len(array) == 0:
-#         return "Invalid grocery list"
-
-#     if s == "":
-#         return "No item check."
-
-#     count = 0
-#     for i in range(0, len(array), 1):
-#         if array[i] == s:
-#             count += 1
-#             sL = str(i)
-
-#     if count == 0:
-#         return "Item is not on your grocery list."
-#     elif count == 1:
-#         return "Item is on grocery list at index " + sL
-#     elif count > 1:
-#         return "Duplicate item on list"
-
-
-# print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "jam"))
-# print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "jam"))
-# print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "butter"))
-# print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], ""))
-# print(findS([], "jam"))
+# Test cases
+print(mergeStrings(["cat", "dog", "fish"], ["one", "two", "three"]))
+print(mergeStrings(["cat", "dog", "fish"], ["one"]))
+print(mergeStrings(["cat", "dog"], ["one", "two"]))
+print(mergeStrings([], ["one", "two", "three"]))
 
 
 #
@@ -120,31 +61,75 @@ Post-conditions; The two passed lists remain unchanged.
 #
 
 '''
-Description; Finds the largest population out of a dici
-Parameters;
-Return;
-Pre-conditions;
+Description; The method takes an array of strings (grocery items in this case) and
+a String and searches the array for a string already in the array that matches it.
+If it finds a match, it returns an affirmation that the item is in the array at
+index n. If not, it returns that the item is not in the array.
+
+Parameters; Array array, String s
+
+Return; String ("Item is not on your grocery list." // or // "Item is on grocery list at index " + sL)
+
+Pre-conditions; array contains only strings, lowercase ,
+
 Post-conditions;
 
-Write any tool that incorporates a dictionary. A dictionary
-is the data structure we explored when pulling data from an API.
-You can find syntax information for Python here,
+'''
 
-https://www.w3schools.com/pytxhon/python_dictionaries.asp
+
+def findS(array, s):
+    # Linear search for a string:
+    # for i in array:
+    #     i.lower()
+
+    if len(array) == 0:
+        return "Invalid grocery list"
+
+    if s == "":
+        return "No item check."
+
+    count = 0
+    for i in range(0, len(array), 1):
+        if array[i] == s:
+            count += 1
+            sL = str(i)
+
+    if count == 0:
+        return "Item is not on your grocery list."
+    elif count == 1:
+        return "Item is on grocery list at index " + sL
+    elif count > 1:
+        return "Duplicate item on list"
+
+
+print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "jam"))
+print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "jam"))
+print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], "butter"))
+print(findS(["eggs", "milk", "bread", "jam", "fruit", "vegetables"], ""))
+print(findS([], "jam"))
+
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+'''
+Description; Prints out the items in a nested dictionary
+Parameters; A nested list
+Return; A printed version of the nested dictionaries
+Pre-conditions; Valid dictionary
+Post-conditions; n/a
 
 '''
 
 
-def vendingMachine():
-
-    vm = {
-        'A1': {'item': "Lays Chips", "price": "1.50"},
-        'A2': {'item': "Ketchup Chips", 'price': "1.75"},
-        'B1': {'item': "Kitkat", 'price': "2.00"},
-        'B2': {'item': "Snickers", 'price': "2.00"},
-        'C1': {'item': "Water", 'price': "2.50"},
-        'C2': {'item': "Milk", 'price': "1.75"}
-    }
+def vendingMachine(array):
 
     for i in vm:
         print(i)
@@ -155,4 +140,12 @@ def vendingMachine():
     print("These are the items currently in stock in the vending machine.")
 
 
-vendingMachine()
+vm = {
+    'A1': {'item': "Lays Chips", "price": "1.50"},
+    'A2': {'item': "Ketchup Chips", 'price': "1.75"},
+    'B1': {'item': "Kitkat", 'price': "2.00"},
+    'B2': {'item': "Snickers", 'price': "2.00"},
+    'C1': {'item': "Water", 'price': "2.50"},
+    'C2': {'item': "Milk", 'price': "1.75"}
+}
+vendingMachine(vm)
